492e99c7935bb24db4a9b160131d67f25acac3fc
sumedhars <sumedharsanjeev@gmail.com>
2022-07-26 09:39:21 +0530

    first commit

---
+++ faceRecognize-liveVideo.py
@@ -0,0 +1,52 @@
+import face_recognition
+import cv2
+import os
+import pickle
+print(cv2.__version__)
+
+Encodings = [] # encoding for different faces that were trained on
+Names = [] 
+matches = []
+name = ''
+
+with open('train_family.pkl','rb') as f:
+    Names = pickle.load(f)
+    Encodings = pickle.load(f)
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# launch camera
+cam = cv2.VideoCapture(0)
+
+while True:
+    # grab the frame from the camera
+    _,frame = cam.read()
+    frameSmall = cv2.resize(frame,(0,0),fx=.20,fy=.20)
+    frameRGB = cv2.cvtColor(frameSmall,cv2.COLOR_BGR2RGB)
+    facePositions = face_recognition.face_locations(frameRGB,model='CNN')
+    allEncodings = face_recognition.face_encodings(frameRGB,facePositions)
+    #step through each found face and compare it to training data encodings
+    for (top,right,bottom,left),face_encoding in zip(facePositions,allEncodings):
+        name = 'Unknown Person'
+        matches = face_recognition.compare_faces(Encodings,face_encoding)
+        #face_encoding is the present unknown face in the frame that we just grabbed, and that is compared to encoding training data
+        #matches will return an array of True and False, True when unknown matches one of the training
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = Names[first_match_index]
+        top = top*5
+        right = right*5
+        bottom = bottom*5
+        left = left*5
+        cv2.rectangle(frame,(left,top),(right,bottom),(0,0,255),2)
+        cv2.putText(frame,name,(left,top-6),font,.75,(0,0,255),2)
+
+    cv2.imshow('Picture',frame)
+    cv2.moveWindow('Picture',0,0)
+
+    # exits application if you press q. opens new live camera if you press 'x' on window
+    if cv2.waitKey(1)==ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
---
+++ trainSave.py
@@ -0,0 +1,27 @@
+import face_recognition
+import cv2
+import os #lets us automate the training
+import pickle
+print(cv2.__version__)
+
+Encodings = []
+Names = [] 
+
+image_dir = '/home/sumedha/faceRecognition-family/familyImages'
+# walk your way through every file in 'known' directory. load the file, do the face encoding, get the name, fill in the encodings+names
+for root, dirs, files in os.walk(image_dir): #starts in image directory, walk through all the files + directories + entire file structure
+    #files -> an array of all of the files in image_dir
+    for file in files:
+        # create the actual complete path with the filename
+        path = os.path.join(root, file) # path with the file
+        name = os.path.splitext(file)[0] #removes the .jpg
+        person = face_recognition.load_image_file(path)
+        encoding = face_recognition.face_encodings(person)[0]
+        Encodings.append(encoding)
+        Names.append(name)
+
+print(Names)
+
+with open('train_family.pkl','wb') as f:
+    pickle.dump(Names,f)
+    pickle.dump(Encodings,f)
